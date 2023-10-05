1767185e29ea2e545fc401a0ba3f7632835063c9
Luizrebelatto <lbrebelatto@gmail.com>
2023-10-05 18:09:35 -0300

    config(iampoor): config project viewcode without storyboard

---
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -0,0 +1,13 @@
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 05/10/23.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
