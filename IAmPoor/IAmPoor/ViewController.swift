8b331b7ff87734ca77a2eba2006f1fa5cae44950
Luizrebelatto <lbrebelatto@gmail.com>
2023-10-06 17:26:16 -0300

    feat(iampoor): create image and set app icons
    - create image
    - create constants

--- IAmPoor/IAmPoor/ViewController.swift
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -19,9 +19,23 @@ class ViewController: UIViewController {
         view.addSubview(label)
         
         label.translatesAutoresizingMaskIntoConstraints = false
-                NSLayoutConstraint.activate([
-                    label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                    label.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
-                ])
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+        ])
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "coal")
+        imageView.contentMode = .scaleAspectFit
+        
+        view.addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
     }
 }
