bedf4c6ab3be2bf2cb550b34b2c2f538eaeadc71
Luizrebelatto <lbrebelatto@gmail.com>
2023-10-06 19:05:18 -0300

    build(iamrich): create projeto and config viewcode without storyboard

---
+++ IamRich/IamRich/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  iamRich
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 06/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
