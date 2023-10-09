b5b916f09a2d2dd4edd38026b6350c39f26cdd11
Luizrebelatto <lbrebelatto@gmail.com>
2023-10-09 17:40:13 -0300

    build(dicee-sorted): create project and config viewcode without storyboard

---
+++ dicee-sorted/dicee-sorted/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dicee-sorted
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 09/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
