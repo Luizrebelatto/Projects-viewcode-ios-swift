91b42dfdc8524df5d43877004c4ef446898543b3
Luizrebelatto <lbrebelatto@gmail.com>
2023-10-09 16:58:01 -0300

    feat(iamrich): implement image

--- IamRich/IamRich/ViewController.swift
+++ IamRich/IamRich/ViewController.swift
@@ -21,10 +21,27 @@ class ViewController: UIViewController {
             titleText.topAnchor.constraint(equalTo: view.topAnchor, constant: 80)
         ])
     }
+    
+    func image() {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "ImageItem")
+        imageView.contentMode = .scaleAspectFit
+        
+        view.addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(named: "AccentColor")
         title()
+        image()
     }
 }
