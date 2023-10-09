f582b47e25c0d662ad7a6b6a9a5e1639a6497b57
Luizrebelatto <lbrebelatto@gmail.com>
2023-10-09 14:35:39 -0300

    feat(iamrich): add text

--- IamRich/IamRich/ViewController.swift
+++ IamRich/IamRich/ViewController.swift
@@ -8,8 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func title() {
+        let titleText = UILabel()
+        titleText.text = "I Am Rich"
+        titleText.textColor = UIColor(named: "colorTitle")
+        titleText.font = UIFont.systemFont(ofSize: 40)
+        view.addSubview(titleText)
+        titleText.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleText.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleText.topAnchor.constraint(equalTo: view.topAnchor, constant: 80)
+        ])
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(named: "AccentColor")
+        title()
     }
 }
