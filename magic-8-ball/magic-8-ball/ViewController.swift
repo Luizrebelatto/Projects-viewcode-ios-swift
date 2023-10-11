d1f1d7e2f96cc0b6de2dcdae975ed072cc6381e4
Luizrebelatto <lbrebelatto@gmail.com>
2023-10-11 19:28:36 -0300

    feat(magic-8-ball): implement title text

--- magic-8-ball/magic-8-ball/ViewController.swift
+++ magic-8-ball/magic-8-ball/ViewController.swift
@@ -8,9 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func label() {
+        let text = UILabel()
+        text.text = "Ask Me Anything..."
+        text.textColor = UIColor(named: "TitleColor")
+        text.font = UIFont.systemFont(ofSize: 40)
+        text.textAlignment = .center
+        view.addSubview(text)
+        
+        text.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            text.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            text.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+        ])
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "Background")
+        label()
         // Do any additional setup after loading the view.
     }
 
