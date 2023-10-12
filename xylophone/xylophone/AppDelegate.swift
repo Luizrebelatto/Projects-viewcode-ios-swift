65fe0b81bbfec0eb59190ddc83cdbd2756e5f799
Luizrebelatto <lbrebelatto@gmail.com>
2023-10-11 23:21:16 -0300

    build(xylophone): create and config viewcode without storyboard

---
+++ xylophone/xylophone/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  xylophone
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 11/10/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool { return true }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+}
---
+++ xylophone/xylophone/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  xylophone
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
