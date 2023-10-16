f68232265a56e865c98461ec4f6121bd82ab04e5
Luizrebelatto <lbrebelatto@gmail.com>
2023-10-15 22:31:36 -0300

    feat(xylophone): colors and buttons
    - implement buttons and adjust your spacings
    - setcolors in assets

--- xylophone/xylophone/ViewController.swift
+++ xylophone/xylophone/ViewController.swift
@@ -8,9 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func createButton(label: String, lineX: CGFloat, lineY: CGFloat, width: CGFloat, height: CGFloat, color: String) {
+        let button = UIButton(frame: CGRect(x: lineX, y: lineY, width: width, height: height))
+        button.backgroundColor = UIColor(named: color)
+        button.setTitle(label, for: .normal)
+        button.layer.cornerRadius = 10
+        view.addSubview(button)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        createButton(label: "C", lineX: 10, lineY: 60, width: 375, height: 90, color: "red")
+        createButton(label: "D", lineX: 18, lineY: 160, width: 360, height: 90, color: "orange")
+        createButton(label: "E", lineX: 26, lineY: 260, width: 345, height: 90, color: "yellow")
+        createButton(label: "F", lineX: 34, lineY: 360, width: 330, height: 90, color: "green")
+        createButton(label: "G", lineX: 42, lineY: 460, width: 315, height: 90, color: "purple")
+        createButton(label: "A", lineX: 50, lineY: 560, width: 300, height: 90, color: "indigo")
+        createButton(label: "B", lineX: 58, lineY: 660, width: 285, height: 90, color: "blue")
     }
 }
